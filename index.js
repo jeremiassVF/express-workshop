1dc1ba4b1e68752fc4d2426c081dbd6d95c1158b
jeremiassVF <jeremiass_warren@outlook.com>
2021-09-24 20:24:36 -0500

    add middleware

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const morgan = require('morgan');
 const express = require('express');
+//middleware
+const auth = require('./middleware/auth');
+const notFound = require('./middleware/notFound');
+const index = require('./middleware/index');
 const app = express();
+//rutes
 const pokemon = require('./routes/pokemon');
 const user = require('./routes/user');
 
@@ -17,17 +22,12 @@ PUT- modificar un recurso
 DELETE- borrar un recurso
 */
 
-app.get("/", (req, res, next) =>{
-    return res.status(200).json({code: 1, menssage: "Bienvenido al Pokédex"});
-});
-
-app.use("/pokemon",pokemon);
+app.get("/", index);
 
 app.use("/user", user);
-
-app.use((req,res,next) =>{
-    return res.status(404).json({code: 404, Message:"URL no encontrada"});
-})
+app.use(auth);
+app.use("/pokemon",pokemon);
+app.use(notFound);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running...");
